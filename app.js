07421b865d46a8ec52457ef7d1d68a3b7ff1c42e
humairahabib123 <humairahabib19@yahoo.com>
2023-02-24 10:24:54 +0500

    10:24 // 2/24/2023

--- app.js
+++ app.js
@@ -15,6 +15,6 @@
 // let name = 1
 let number = +prompt("Write your name here")
 
-for (let i = 1; i <= 10; i++) {
-    document.write(`<table border="1"> <tr> <td>${number}</td> <td>X</td> <td>${i} </td><td>=</td> <td>${number * i}</td><br /></tr></table>`)   
+for (let i = 1; i <= 10000; i++) {
+    console.log(` ${number} X ${i} = ${number * i}<br />`)   
 }
